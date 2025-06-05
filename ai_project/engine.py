ec7c8d9bcf673bf59942dc4ad823494af8fde835
trevorxarcieri <agent.steel.ta@gmail.com>
2025-06-05 18:19:51 -0400

    Moved abstract board class to its own file.

--- ai_project/engine.py
+++ ai_project/engine.py
@@ -2,53 +2,10 @@
 
 import random
 from abc import ABC, abstractmethod
-from collections.abc import Generator
-from enum import Enum
 from math import inf
 from typing import Generic, TypeVar
 
-MoveT = TypeVar("MoveT")
-
-
-class AbstractPlayer(Enum):
-    """Enum for players."""
-
-    MAX = 1
-    MIN = -1
-
-
-class AbstractBoard(ABC, Generic[MoveT]):
-    """Abstract base class for a board."""
-
-    turn: AbstractPlayer
-
-    @property
-    @abstractmethod
-    def legal_moves(self) -> Generator[MoveT, None, None]:
-        """All legal moves for the current player."""
-        pass
-
-    @property
-    @abstractmethod
-    def is_draw(self) -> bool:
-        """Check if the game is a draw."""
-        pass
-
-    @property
-    def game_over(self) -> bool:
-        """Returns ``True`` if the game is over."""
-        return self.is_draw or not bool(list(self.legal_moves))
-
-    @abstractmethod
-    def push(self, move: MoveT) -> None:
-        """Apply a move to the board."""
-        pass
-
-    @abstractmethod
-    def pop(self) -> None:
-        """Undo the last move applied to the board."""
-        pass
-
+from ai_project.board import AbstractBoard, AbstractPlayer, MoveT
 
 BoardT = TypeVar("BoardT", bound="AbstractBoard")
 
